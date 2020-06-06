c37a28882a3f0fcf8e5fe9558e6166cc9121b33e
Harry Liu <harryliu1995@gmail.com>
2020-06-05 20:36:32 -0400

    comment out admin access

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -4,9 +4,9 @@ from flask_admin.contrib.sqla import ModelView
 from WebApp.config import Config
 
 
-class ModelView(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.username in Config.ADMINS
+# class ModelView(ModelView):
+#     def is_accessible(self):
+#         return current_user.is_authenticated and current_user.username in Config.ADMINS
 
 
 class MyAdminIndexView(AdminIndexView):
@@ -14,5 +14,5 @@ class MyAdminIndexView(AdminIndexView):
     def index(self):
         return self.render("admin/index.html")
 
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.username in Config.ADMINS
+    # def is_accessible(self):
+    #     return current_user.is_authenticated and current_user.username in Config.ADMINS
