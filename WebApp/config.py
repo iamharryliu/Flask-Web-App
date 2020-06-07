4d1c3f2dc337f261af6c2e5a7df418087f9ef2bb
Harry Liu <harryliu1995@gmail.com>
2020-06-07 09:58:31 -0400

    update

--- WebApp/config.py
+++ WebApp/config.py
@@ -1,7 +1,6 @@
 import os
 import json
 
-# We want the database path to be in the main directory if we are using SQLite in order to work with Flask-Migrate.
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 ROOT_PATH = os.path.abspath(os.path.join(DIR_PATH, os.pardir))
 
@@ -12,15 +11,8 @@ with open("/etc/config.json") as config_file:
 class Config:
 
     SECRET_KEY = config.get("SECRET_KEY")
-    DEBUG = True
-
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{ROOT_PATH}/site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    RECAPTCHA_USE_SSL = config.get("RECAPTCHA_USE_SSL")
-    RECAPTCHA_PUBLIC_KEY = config.get("RECAPTCHA_PUBLIC_KEY")
-    RECAPTCHA_PRIVATE_KEY = config.get("RECAPTCHA_PRIVATE_KEY")
-    RECAPTCHA_OPTIONS = config.get("RECAPTCHA_OPTIONS")
 
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
@@ -29,4 +21,11 @@ class Config:
     MAIL_PASSWORD = config.get("EMAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = config.get("EMAIL_DEFAULT_SENDER")
 
+    RECAPTCHA_USE_SSL = config.get("RECAPTCHA_USE_SSL")
+    RECAPTCHA_PUBLIC_KEY = config.get("RECAPTCHA_PUBLIC_KEY")
+    RECAPTCHA_PRIVATE_KEY = config.get("RECAPTCHA_PRIVATE_KEY")
+    RECAPTCHA_OPTIONS = config.get("RECAPTCHA_OPTIONS")
+
     ADMINS = config.get("ADMINS")
+
+    DEBUG = True
