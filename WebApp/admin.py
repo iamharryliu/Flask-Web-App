2080facd3a739836240a44a2a0e72728f28e2ca4
Harry <harryliu1995@gmail.com>
2020-07-23 13:47:56 -0400

    remove commented code and enable admin view privelige

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -4,15 +4,10 @@ from flask_admin.contrib.sqla import ModelView
 from WebApp.config import Config
 
 
-# class ModelView(ModelView):
-#     def is_accessible(self):
-#         return current_user.is_authenticated and current_user.username in Config.ADMINS
-
-
 class MyAdminIndexView(AdminIndexView):
     @expose("/")
     def index(self):
         return self.render("admin/index.html")
 
-    # def is_accessible(self):
-    #     return current_user.is_authenticated and current_user.username in Config.ADMINS
+    def is_accessible(self):
+        return current_user.username in Config.ADMINS
